36961c014a7758813cf19996552f5cd5f6020c92
Yilian Zhang <yilianz@usca.edu>
2021-01-11 16:26:45 -0500

    update C#

---
+++ test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSCI330code
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from C#!");
+        }
+    }
+}
