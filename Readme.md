18d02c1b6b6ed00e3e6c5c4a7dfc8b59f43dfc34
Yilian Zhang <yilianz@usca.edu>
2021-01-11 15:55:24 -0500

    copy files from master to main

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
-#file collection
+## This is the markdown file 
+
+Collection of hello world file in different languages
+
+* HTML
+* js
+* python 
+* java
+* c#
+* c++
+* php
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // file including function declarations for standard I/O​
+
+int main(){
+    printf("Hello World from C!\n");   // prints a message with a carriage return​
+
+    return 0;   // return value of function - end of program​
+}
